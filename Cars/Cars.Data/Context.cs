01ec5c78716205e0310f0c7cbfdb5588adc4e125
student <student@KIRP-404-STUD11>
2016-11-18 11:32:41 +0300

    AddEntityContext

---
+++ Cars/Cars.Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Data
+{
+    class Context : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public Context() : base("localsql") { }
+    }
+}
