d159d01abe898ae6e1800286ce7f7b24ab6a479c
student <student@KIRP-404-STUD03>
2016-11-18 11:08:42 +0300

    Add Models

---
+++ Cars/Cars.Data/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Data
+{
+    class Car
+    {
+        public int Id { get; set; }
+        public DateTime Year { get; set; }
+        public string Color { get; set; }
+
+        public Model Model { get; set; }
+       
+    }
+}
